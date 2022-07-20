f4706edffb8d0d497ff425063892654422af90ee
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-07-20 19:06:24 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CRUD_project.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        [DisplayName("FirstName")]
+        public string FirstName { get; set; } = null!;
+        [DisplayName("LastName")]
+        public string? LastName { get; set; }
+    }
+}
